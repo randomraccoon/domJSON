253e86dce1d01fbe202922ed61c6ce56b79758df
Troy Amelotte <troy.amelotte@gTAef49b1.local>
2017-08-31 08:16:42 -0700

    init

---
+++ app.js
@@ -0,0 +1,33 @@
+let users = [
+  {
+    firstName: "troy",
+    lastName: "amelotte",
+    age: 12,
+    email: "troy@amelotte.com",
+    location: "PHX"
+  },
+  {
+    firstName: "kevin",
+    lastName: "bacon",
+    age: 13,
+    email: "kevin@amelotte.com",
+    location: "LA"
+  },
+  {
+    firstName: "bjorn",
+    lastName: "luke",
+    age: 20,
+    email: "asdf@asdf.com",
+    location: "NY"
+  },
+  {
+    firstName: "rahul",
+    lastName: "lastname",
+    age: 23,
+    email: "ra@hul.com",
+    location: "PHX"
+  }
+]
+
+// Use the list of users above. Populate the unordered list with all the users
+// that are located in phx. Then populate the table below it with all the users.
