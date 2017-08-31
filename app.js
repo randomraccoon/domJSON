251c42ed8786252e7f0fb3f7c8eff2e385e5a72e
Cameron Earl <developer.cameron@gmail.com>
2017-08-31 10:28:35 -0700

    Complete exercise

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ let users = [
   },
   {
     firstName: "rahul",
-    lastName: "lastname",
+    lastName: "salota",
     age: 23,
     email: "ra@hul.com",
     location: "PHX"
@@ -31,3 +31,33 @@ let users = [
 
 // Use the list of users above. Populate the unordered list with all the users
 // that are located in phx. Then populate the table below it with all the users.
+
+const UL = document.querySelector('ul');
+const TBODY = document.querySelector('tbody');
+
+
+window.onload = ()=> {
+  for(let obj of users) {
+    if (obj.location === "PHX") {
+      let li = document.createElement('li');
+      let fullName = capitalize(obj.firstName) + " " + capitalize(obj.lastName);
+      let text = document.createTextNode(fullName);
+      li.appendChild(text);
+      UL.appendChild(li);
+    }
+    let tr = document.createElement('tr');
+    for (let key in obj) {
+      let td = document.createElement('td');
+      let val = /name/i.test(key) ? capitalize(obj[key]) : obj[key];
+      let text = document.createTextNode(val);
+      td.appendChild(text);
+      tr.appendChild(td);
+    }
+    TBODY.appendChild(tr);
+  }
+
+};
+
+const capitalize = (str) => {
+  return str[0].toUpperCase() + str.slice(1);
+}
